4212e8b5d5bca7dd8132e337793c414d76ca1541
김신아 <sinaskim@naver.com>
2022-02-08 19:44:29 +0900

    event commit

---
+++ prwebsite/static/lion/js/event.js
@@ -0,0 +1,20 @@
+const slides_container = document.querySelector(".my_slider_list");
+const slides_content = document.querySelectorAll(".slider_content");
+const moving_button = document.querySelector(".moving_header_bar_ofdot");
+const pre = document.querySelector(".pre_button");
+const next = document.querySelector(".next_button");
+const slide_length = slides_content.length;
+let nowslide = 0; 
+
+function skipSlider(num) {
+    moving_button.style.left = num * 410+'px';
+    slides_container.style.left = - num * 600 + 'px'; 
+    nowslide = num;
+}
+
+pre.addEventListener("click",function(){
+    if(nowslide !== 0){skipSlider(nowslide-1)}});
+next.addEventListener("click",function(){
+    if(nowslide !== slide_length-1){skipSlider(nowslide+1)}});
+
+console.log(slide_length);
