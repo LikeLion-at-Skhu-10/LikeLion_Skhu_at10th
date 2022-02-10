f4f2cf118d693e113057e3f11c7d32cdd2ae7a63
김신아 <sinaskim@naver.com>
2022-02-10 13:25:45 +0900

    bar remove and name align

--- prwebsite/static/lion/js/event.js
+++ prwebsite/static/lion/js/event.js
@@ -1,13 +1,11 @@
 const slides_container = document.querySelector(".my_slider_list");
 const slides_content = document.querySelectorAll(".slider_content");
-const moving_button = document.querySelector(".moving_header_bar_ofdot");
 const pre = document.querySelector(".pre_button");
 const next_button = document.querySelector(".next_button");
 const slide_length = slides_content.length;
 let nowslide = 0; 
 
 function skipSlider(num) {
-    moving_button.style.left = num * 410+'px';
     slides_container.style.left = - num * 600 + 'px'; 
     nowslide = num;
 }
