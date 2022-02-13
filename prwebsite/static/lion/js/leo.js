7e7fcb19eb6c8105d1d020e77144bd45eeb60b21
강민정 <staccato189@gmail.com>
2022-02-14 01:48:13 +0900

    검색창 수정

--- prwebsite/static/lion/js/leo.js
+++ prwebsite/static/lion/js/leo.js
@@ -73,45 +73,45 @@ function out(leo_footer) {
 
 function changeVI() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_one" href="#leo_VI" style="text-decoration:none;">소개영상</a>';
+    element.innerHTML = '<a id="leo_one" href="#leo_VI" style="text-decoration:none;">소개영상 이동</a>';
 }
 
 function changeIN() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_two" href="#leo_IN" style="text-decoration:none;">멋사소개</a>';
+    element.innerHTML = '<a id="leo_two" href="#leo_IN" style="text-decoration:none;">멋사소개 이동</a>';
 }
 
 function changeRE() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_three" href="#leo_RE" style="text-decoration:none;">모집대상</a>';
+    element.innerHTML = '<a id="leo_three" href="#leo_RE" style="text-decoration:none;">모집대상 이동</a>';
 }
 
 function changeSC() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_four" href="#leo_SC" style="text-decoration:none;">모집일정</a>';
+    element.innerHTML = '<a id="leo_four" href="#leo_SC" style="text-decoration:none;">모집일정 이동</a>';
 }
 
 function changeMU() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_five" href="#leo_MU" style="text-decoration:none;">규칙 & 커리큘럼</a>';
+    element.innerHTML = '<a id="leo_five" href="#leo_MU" style="text-decoration:none;">규칙 & 커리큘럼 이동</a>';
 }
 
 function changeSL() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_six" href="#leo_SL" style="text-decoration:none;">지난해 프로젝트</a>';
+    element.innerHTML = '<a id="leo_six" href="#leo_SL" style="text-decoration:none;">지난해 프로젝트 이동</a>';
 }
 
 function changeEV() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_seven" href="#leo_EV" style="text-decoration:none;">이벤트 참여</a>';
+    element.innerHTML = '<a id="leo_seven" href="#leo_EV" style="text-decoration:none;">이벤트 참여 이동</a>';
 }
 
 function changeMA() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_eight" href="#leo_MA" style="text-decoration:none;">운영진들</a>';
+    element.innerHTML = '<a id="leo_eight" href="#leo_MA" style="text-decoration:none;">운영진들 이동</a>';
 }
 
 function changeFO() {
     const element = document.getElementById("leo_title");
-    element.innerHTML = '<a id="leo_nine" href="#leo_FO" style="text-decoration:none;">멋사 정보</a>';
+    element.innerHTML = '<a id="leo_nine" href="#leo_FO" style="text-decoration:none;">멋사정보 이동</a>';
 }
